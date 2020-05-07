065b1f89ec391b936ef0c7f151c1dfb6947810bb
Istvan Csomortani <istvan.csomortani@analog.com>
2020-05-07 11:23:35 +0100

    ad77681evb: spi_clk is asynchronous

--- projects/ad77681evb/common/ad77681evb_qsys.tcl
+++ projects/ad77681evb/common/ad77681evb_qsys.tcl
@@ -1,4 +1,4 @@
-  
+
     # dma
 
     add_instance axi_dmac_0 axi_dmac
@@ -22,20 +22,21 @@
     add_connection sys_clk.clk_reset axi_dmac_0.s_axi_reset
     add_connection sys_dma_clk.clk axi_dmac_0.m_dest_axi_clock
     add_connection sys_dma_clk.clk_reset axi_dmac_0.m_dest_axi_reset
-   	
+
 	# create a SPI Engine architecture for ADC
-	
+
     # axi_spi_engine
 
     add_instance axi_spi_engine_0 axi_spi_engine
-    set_instance_parameter_value axi_spi_engine_0 {DATA_WIDTH}  {32}
-    set_instance_parameter_value axi_spi_engine_0 {MM_IF_TYPE}  {0}
-    set_instance_parameter_value axi_spi_engine_0 {NUM_OF_SDI}  {1}
-    set_instance_parameter_value axi_spi_engine_0 {NUM_OFFLOAD} {1}
+    set_instance_parameter_value axi_spi_engine_0 {ASYNC_SPI_CLK} {1}
+    set_instance_parameter_value axi_spi_engine_0 {DATA_WIDTH}    {32}
+    set_instance_parameter_value axi_spi_engine_0 {MM_IF_TYPE}    {0}
+    set_instance_parameter_value axi_spi_engine_0 {NUM_OF_SDI}    {1}
+    set_instance_parameter_value axi_spi_engine_0 {NUM_OFFLOAD}   {1}
 
     add_connection sys_dma_clk.clk axi_spi_engine_0.if_spi_clk
     add_connection sys_clk.clk axi_spi_engine_0.s_axi_clock
-    add_connection sys_clk.clk_reset axi_spi_engine_0.s_axi_reset	
+    add_connection sys_clk.clk_reset axi_spi_engine_0.s_axi_reset
 
     # spi_engine_execution
 
@@ -55,7 +56,7 @@
     set_interface_property spi_engine_sdo EXPORT_OF spi_engine_execution_0.if_sdo
     add_interface spi_engine_sdo_t conduit end
     set_interface_property spi_engine_sdo_t EXPORT_OF spi_engine_execution_0.if_sdo_t
-   
+
     # spi_engine_interconnect
 
     add_instance spi_engine_interconnect_0 spi_engine_interconnect
@@ -68,9 +69,10 @@
     # spi_engine_offload
 
     add_instance spi_engine_offload_0 spi_engine_offload
-    set_instance_parameter_value spi_engine_offload_0 {ASYNC_TRIG} {1}
-    set_instance_parameter_value spi_engine_offload_0 {DATA_WIDTH} {32}
-    set_instance_parameter_value spi_engine_offload_0 {NUM_OF_SDI} {1}
+    set_instance_parameter_value spi_engine_offload_0 {ASYNC_TRIG}    {1}
+    set_instance_parameter_value spi_engine_offload_0 {ASYNC_SPI_CLK} {1}
+    set_instance_parameter_value spi_engine_offload_0 {DATA_WIDTH}    {32}
+    set_instance_parameter_value spi_engine_offload_0 {NUM_OF_SDI}    {1}
 
     add_connection sys_dma_clk.clk spi_engine_offload_0.if_ctrl_clk
     add_connection sys_dma_clk.clk spi_engine_offload_0.if_spi_clk
@@ -81,24 +83,24 @@
     # SPI Engine connections
 
     add_connection axi_spi_engine_0.cmd spi_engine_interconnect_0.s0_cmd
-    add_connection axi_spi_engine_0.sdo_data spi_engine_interconnect_0.s0_sdo 
-    add_connection spi_engine_execution_0.sdi_data spi_engine_interconnect_0.m_sdi 
+    add_connection axi_spi_engine_0.sdo_data spi_engine_interconnect_0.s0_sdo
+    add_connection spi_engine_execution_0.sdi_data spi_engine_interconnect_0.m_sdi
     add_connection spi_engine_execution_0.sync spi_engine_interconnect_0.m_sync
-    add_connection spi_engine_interconnect_0.m_cmd spi_engine_execution_0.cmd 
-    add_connection spi_engine_interconnect_0.m_sdo spi_engine_execution_0.sdo_data 
-    add_connection spi_engine_interconnect_0.s0_sdi axi_spi_engine_0.sdi_data 
-    add_connection spi_engine_interconnect_0.s0_sync axi_spi_engine_0.sync 
-    add_connection spi_engine_interconnect_0.s1_sdi spi_engine_offload_0.sdi_data 
+    add_connection spi_engine_interconnect_0.m_cmd spi_engine_execution_0.cmd
+    add_connection spi_engine_interconnect_0.m_sdo spi_engine_execution_0.sdo_data
+    add_connection spi_engine_interconnect_0.s0_sdi axi_spi_engine_0.sdi_data
+    add_connection spi_engine_interconnect_0.s0_sync axi_spi_engine_0.sync
+    add_connection spi_engine_interconnect_0.s1_sdi spi_engine_offload_0.sdi_data
     add_connection spi_engine_interconnect_0.s1_sync spi_engine_offload_0.sync
-    add_connection spi_engine_offload_0.cmd spi_engine_interconnect_0.s1_cmd 
+    add_connection spi_engine_offload_0.cmd spi_engine_interconnect_0.s1_cmd
     add_connection spi_engine_offload_0.ctrl_cmd_wr axi_spi_engine_0.offload0_cmd
     add_connection spi_engine_offload_0.ctrl_sdo_wr axi_spi_engine_0.offload0_sdo
     add_connection spi_engine_offload_0.if_ctrl_enable axi_spi_engine_0.if_offload0_enable
     add_connection spi_engine_offload_0.if_ctrl_enabled axi_spi_engine_0.if_offload0_enabled
     add_connection spi_engine_offload_0.if_ctrl_mem_reset axi_spi_engine_0.if_offload0_mem_reset
     add_connection spi_engine_offload_0.sdo_data spi_engine_interconnect_0.s1_sdo
-    add_connection spi_engine_offload_0.offload_sdi axi_dmac_0.s_axis	
-	
+    add_connection spi_engine_offload_0.offload_sdi axi_dmac_0.s_axis
+
     # cpu interconnects
 
     ad_cpu_interconnect 0x00020000 axi_dmac_0.s_axi
